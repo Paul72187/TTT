26e0531d2132b5065b2740db52e3a5f6f2b1aadf
Paul Cooper <paulcooper@Pauls-MacBook-Air.local>
2022-06-18 17:31:42 +0100

    Square and Board features added

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,8 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css";
+
+const Square = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "gold",
+        margin: 10,
+        padding: 20,
+      }}
+    >
+      Square
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "skyblue",
+        margin: 10,
+        padding: 20,
+      }}
+    >
+      Board
+      <Square />
+    </div>
+  );
+};
+
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "salmon",
+        margin: 10,
+        padding: 20,
+      }}
+    >
+      Game
+      <Board />
+    </div>
+  );
 };
 
 ReactDOM.render(<Game />, document.getElementById("root"));
